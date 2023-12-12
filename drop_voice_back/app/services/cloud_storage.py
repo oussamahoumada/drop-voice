ff1d0a8303a4673c26bb91c16e810da012a583c6
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2023-12-12 17:59:29 +0100

    fix dings

--- drop_voice_back/app/services/cloud_storage.py
+++ drop_voice_back/app/services/cloud_storage.py
@@ -6,9 +6,15 @@ from dropbox.exceptions import AuthError
 ####################
 ## add to dropbox ##
 ####################
-dbx = dropbox.Dropbox( app_key = os.getenv('APP_KEY_DROPBOX'), app_secret = os.getenv('APP_SECRET_DROPBOX'), oauth2_refresh_token=os.getenv('REFRESH_TOKEN_DROPBOX'))
+load_dotenv()
 
-def upload_file(access_token, file_from,file_to):
+app_key_dropbox = os.getenv('APP_KEY_DROPBOX')
+app_secret = os.getenv('APP_SECRET_DROPBOX')
+oauth2_refresh_token = os.getenv('REFRESH_TOKEN_DROPBOX')
+
+dbx = dropbox.Dropbox(app_key=app_key_dropbox, app_secret=app_secret, oauth2_refresh_token=oauth2_refresh_token)
+
+def upload_file(file_from, file_to):
 
     if(file_from!='' and file_to!=''):
         with open(file_from, "rb") as f:
@@ -18,10 +24,8 @@ def upload_file(access_token, file_from,file_to):
         print("empty variable error")
 
 def add_to_dropbox(path, name):
-    load_dotenv()
-    file_to = '/test/'+name
-    access_token = os.getenv('DROPBOX_ACCESS_TOKEN')
-    upload_file(access_token, path, file_to)
+    file_to = '/test/' + name
+    upload_file(path, file_to)
 
 
 ######################
@@ -30,8 +34,9 @@ def add_to_dropbox(path, name):
 
 def get_image_url(file_path):
     try:
-        shared_link = dbx.sharing_create_shared_link("/test/"+file_path)
+        shared_link = dbx.sharing_create_shared_link("/test/" + file_path)
         raw_url = shared_link.url.replace('dl=0', 'raw=1')
+
         return raw_url
 
     except AuthError as e:
