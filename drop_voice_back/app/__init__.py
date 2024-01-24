313be4b9ad0c628247ddf2a81cbf86fef963ae9e
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2024-01-24 20:12:51 +0100

    update port for lunch a flask in docker-compose.prod and in __init__

--- drop_voice_back/app/__init__.py
+++ drop_voice_back/app/__init__.py
@@ -38,6 +38,6 @@ if os.getenv('ENV') == 'development' and (is_exist_cert and is_exist_key):
 
     sslify = SSLify(app, permanent=True)
 
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=context)
+    app.run(debug=True, host='0.0.0.0', ssl_context=context)
 else :
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
