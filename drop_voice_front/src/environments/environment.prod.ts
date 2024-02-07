2ff9edb3429d85c3755a30f4eb691b2dad1b429b
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2024-02-07 20:18:38 +0100

    update nginx conf for add lcoation api

--- drop_voice_front/src/environments/environment.prod.ts
+++ drop_voice_front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://flaskdocker:5000',
+  apiUrl: '/api',
   mapbox: {
     accessToken: 'pk.eyJ1Ijoicm9kcmlnb2thbWFkYSIsImEiOiJjbGZ5NTVhenAwanBzM3Fta3Y3b29temE5In0.PkdHrkHBrx9RALYhyLMRxA',
   },
